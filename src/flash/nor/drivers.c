a0f8a41b95dc51c02f518f96e2144523a617f7af
Salvatore Messina <salvatore.messina@st.com>
2023-11-23 16:35:24 +0100

    Added SR6P3 device support and memory driver
    Signed-off-by: Salvatore Messina <salvatore.messina@st.com>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -88,6 +88,7 @@ extern const struct flash_driver spc58xn_flash;
 extern const struct flash_driver sr5_flash;
 extern const struct flash_driver sr6_flash;
 extern const struct flash_driver sr6p6_flash;
+extern const struct flash_driver sr6p3_flash;
 
 /**
  * The list of built-in flash drivers.
@@ -173,6 +174,7 @@ static const struct flash_driver * const flash_drivers[] = {
 	&sr5_flash,
 	&sr6_flash,
 	&sr6p6_flash,
+	&sr6p3_flash,
 	NULL,
 };
 
