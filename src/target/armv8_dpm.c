e452889c88044e887686cc752d544518644c7abd
Antonio Borneo <borneo.antonio@gmail.com>
2024-05-13 17:20:52 +0200

    target: armv8_dpm: silence error on register R/W
    The command 'gdb_report_register_access_error' is used to silence
    errors while reading registers and not reporting them to GDB.
    Nevertheless, the error is printed by a LOG_ERROR() in armv8_dpm.
    
    Change the message to LOG_DEBUG().
    It will still cause the error to be propagated and eventually
    printed by the caller (e.g. by the command 'reg').
    
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-by: Antonio Maria BORNEO <antonio.borneo@foss.st.com>
    Reviewed-by: Tarek BOCHKATI <tarek.bouchkati@st.com>
    Tested-by: Antonio Maria BORNEO <antonio.borneo@foss.st.com>
    Tested-by: Tarek BOCHKATI <tarek.bouchkati@st.com>

--- src/target/armv8_dpm.c
+++ src/target/armv8_dpm.c
@@ -677,7 +677,7 @@ static int dpmv8_read_reg(struct arm_dpm *dpm, struct reg *r, unsigned int regnu
 	}
 
 	if (retval != ERROR_OK)
-		LOG_ERROR("Failed to read %s register", r->name);
+		LOG_DEBUG("Failed to read %s register", r->name);
 
 	return retval;
 }
@@ -719,7 +719,7 @@ static int dpmv8_write_reg(struct arm_dpm *dpm, struct reg *r, unsigned int regn
 	}
 
 	if (retval != ERROR_OK)
-		LOG_ERROR("Failed to write %s register", r->name);
+		LOG_DEBUG("Failed to write %s register", r->name);
 
 	return retval;
 }
