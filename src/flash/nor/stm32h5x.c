4121cc188fbfe454f69258c74620df5ba2d6fd0d
HAOUES Ahmed <ahmed.haoues@st.com>
2025-11-03 10:16:57 +0100

    flash/stm32h5x: Support STM32H54/H55x Devices
    STM32H54/H55x devices are quite similar to H56/h57x
    devices with flash size equal to 1Mb
    
    Signed-off-by: HAOUES Ahmed <ahmed.haoues@st.com>
    Reviewed-by: Tarek BOCHKATI <tarek.bouchkati@st.com>

--- src/flash/nor/stm32h5x.c
+++ src/flash/nor/stm32h5x.c
@@ -127,6 +127,10 @@ static const struct stm32h5x_rev stm32h5e_h5fxx_revs[] = {
 	{ 0x1000, "A" }, { 0x1001, "Z" }, NO_MORE_REVS
 };
 
+static const struct stm32h5x_rev stm32h54_h55xx_revs[] = {
+	{ 0x1000, "A" }, { 0x1001, "Z" },  { 0x1007, "X" }, NO_MORE_REVS
+};
+
 /* Devices identities */
 static const struct stm32h5x_dev_info stm32h5x_dev_info_db[] = {
 	{
@@ -168,6 +172,17 @@ static const struct stm32h5x_dev_info stm32h5x_dev_info_db[] = {
 		.revs				= stm32h5e_h5fxx_revs,
 		.flags				= F_HAS_DUAL_BANK | F_HAS_TZ,
 		.max_flash_size_kb	= 4096,
+		.flash_regs_base	= 0x40022000,
+		.flash_size_addr	= 0x08FFF80C,
+		.wps_group_size		= 4,
+		.wps_mask			= 0xFFFFFFFF,
+	},
+	{
+		.id					= DEVID_STM32H54_H55XX,
+		.name				= "STM32H54/H55xx",
+		.revs				= stm32h54_h55xx_revs,
+		.flags				= F_HAS_DUAL_BANK | F_HAS_TZ,
+		.max_flash_size_kb	= 1024,
 		.flash_regs_base	= 0x40022000,
 		.flash_size_addr	= 0x08FFF80C,
 		.wps_group_size		= 4,
@@ -1231,6 +1246,7 @@ static int stm32h5x_probe(struct flash_bank *bank)
 	case DEVID_STM32H52_H53XX:
 	case DEVID_STM32H56_H57XX:
 	case DEVID_STM32H5E_H5FXX:
+	case DEVID_STM32H54_H55XX:
 		/* Always dual-bank */
 		stm32h5x_bank->dual_bank = true;
 		sector_size_kb = 8;
--- src/flash/nor/stm32h5x.h
+++ src/flash/nor/stm32h5x.h
@@ -93,6 +93,7 @@
 #define DEVID_STM32H56_H57XX	0x484    /* RM0481 */
 #define DEVID_STM32H52_H53XX	0x478    /* RM0481 */
 #define DEVID_STM32H5E_H5FXX	0x47A    /* RM0517 */
+#define DEVID_STM32H54_H55XX	0x47C    /* RM0539 */
 
 /* Known Flash base addresses */
 #define STM32_FLASH_BANK_BASE	0x08000000
